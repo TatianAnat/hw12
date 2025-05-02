6b7c2d9c730b8d002b90ce56635bff7b69583843
TatianAnat <TatianAnat@yandex.ru>
2025-05-02 17:05:57 +0400

    homework 12 task 3

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,14 @@ public class Main {
         //task 2
         System.out.println("Task 2");
         System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        //task 3
+        System.out.println("Task 3");
+        String firstName3 = "Семён";
+        String middleName3 = "Семёнович";
+        String lastName3 = "Иванов";
+        String fullName3 = lastName3 + " " + firstName3 + " " + middleName3;
+        System.out.println("Данные Ф.И.О. сотрудника — " + fullName3.replace("ё","е"));
+
     }
 }
